feaade4346b880baaec4e6e81258ee35402dcf23
aman <aman282571@gmail.com>
2022-12-20 13:29:49 +0530

    rem

--- src/components/About.js
+++ src/components/About.js
@@ -1,48 +1,40 @@
-import React from "react";
-import "../cssfiles/About.css";
-import me from "./me.jpg";
+import React from 'react';
+import '../cssfiles/About.css';
+import me from './me.jpg';
 function About() {
-  return (
-    <div id="about">
-      <div className="img">
-        {" "}
-        <div className="image" style={{ backgroundImage: `url(${me})` }}></div>
-      </div>
+	return (
+		<div id='about'>
+			<div className='img'>
+				{' '}
+				<div className='image' style={{ backgroundImage: `url(${me})` }}></div>
+			</div>
 
-      <div className="about">
-        <h1>Hi, I'm Aman </h1>
-        <p className="content">
-          I am a sophomore at IIT Roorkee. Intersted in building full stack
-          projects with MERN stack. Apart from development, I also do CP on{" "}
-          <a
-            href="https://leetcode.com/aman282571/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Leetcode
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://www.codechef.com/users/aman282571"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Codechef
-          </a>{" "}
-          platforms.
-          {/* <p className="cv">
-            <a
-              href="https://drive.google.com/file/d/12IS1UO73cp8xTbbrDx3-JICDGnd22thz/view?usp=sharing"
-              target="_blank"
-              rel="noreferrer"
-            >
-              My CV
-            </a>
-          </p> */}
-        </p>
-      </div>
-    </div>
-  );
+			<div className='about'>
+				<h1>Hi, I'm Aman </h1>
+				<p className='content'>
+					I am a final year student at IIT Roorkee. Intersted in building full
+					stack projects with MERN stack. Apart from development, I also do CP
+					on{' '}
+					<a
+						href='https://leetcode.com/aman282571/'
+						target='_blank'
+						rel='noreferrer'
+					>
+						Leetcode
+					</a>{' '}
+					and{' '}
+					<a
+						href='https://codeforces.com/profile/aman282571'
+						target='_blank'
+						rel='noreferrer'
+					>
+						Codeforces
+					</a>{' '}
+					platforms.
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default About;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,194 +1,138 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-import "../cssfiles/Projects.css";
-import $ from "jquery";
+import '../cssfiles/Projects.css';
+import $ from 'jquery';
 
 function Projects() {
-  useEffect(() => {
-    if (document.documentElement.clientWidth > 600) {
-      $(".desc").slideDown();
-      $(".heading").toggleClass("heading1");
-    }
-    $(".heading").on("click", (e) => {
-      $(e.target).toggleClass("heading1");
-      $(e.target).siblings(".desc").slideToggle();
-    });
-  }, []);
-  return (
-    <div id="projects">
-      <h1>Projects</h1>
-      <div className="pro_container">
-        <div className="project">
-          <p className="heading">Blogs App</p>
-          <div className="desc">
-            Blogs app where user can create,edit and delete the blogs and their
-            profile. User can also see others profiles and blogs.
-            <p className="techstak">
-              <span>Tools:-</span> React and Express.js..
-            </p>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://blogsapp28.herokuapp.com/"
-            >
-              Open Website
-            </a>
-          </div>
-        </div>
-        <div className="project">
-          <p className="heading">Fan Club portal</p>
-          <div className="desc">
-            Portal includes authentication and Oauth, profile page(user can edit
-            info),leaderboard (to show the most active user),chat room(admin can
-            change the group fields and make others admin + real time chat +
-            highlight the unread msgs). User can make room,follow and unfollow
-            rooms.
-            <p className="techstak">
-              <span>Tools:-</span> HTML, CSS, JS, jquery, Nodejs, Expressjs,
-              Mongodb, and socket.io..
-            </p>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/aman282571/fan_club_portal"
-            >
-              go to github
-            </a>
-          </div>
-        </div>
-        <div className="project">
-          <p className="heading">Codeforces Problems</p>
-          <div className="desc">
-            User can view all problems of codeforces. <br /> Fetaure includes
-            pagination and filter by tags, status(Todo,tried solved),
-            name,increasing and decreasing order of rating and solved count.
-            <p className="techstak">
-              <span>Tools:-</span> React..
-            </p>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://aman282571.github.io/cf_problems/"
-            >
-              Open website
-            </a>
-          </div>
-        </div>
-        <div className="project">
-          <p className="heading">Sudoku Solver</p>
-          <div className="desc">
-            This sudoku solver takes the input from the user and validate the
-            sudoku and if any error found then it highlights those errors
-            otherwise shows all possible solution of that sudoku.
-            <p className="techstak">
-              <span>Tools:-</span> React..
-            </p>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://aman282571.github.io/sudoku"
-            >
-              Open website
-            </a>
-          </div>
-        </div>
-        <div className="project">
-          <p className="heading">E-Library</p>
-          <div className="desc">
-            Library which includes registration, login page for each user. Users
-            can add, search,borrow(owner of the book will get mail),delete books
-            from this. Individual webpages to show each book.
-            <p className="techstak">
-              <span>Tools:-</span>
-              HTML, CSS, JS, Nodejs, Expressjs, Mongodb..
-            </p>
-            <a
-              href="https://github.com/aman282571/library"
-              target="_blank"
-              rel="noreferrer"
-            >
-              go to github
-            </a>
-          </div>
-        </div>
-        <div className="project">
-          <p className="heading">Basic Banking System</p>
-          <div className="desc">
-            Basic banking system which supports transaction between multiple
-            users.
-            <p className="techstak">
-              <span>Tools:-</span>
-              HTML, CSS, jquery, Nodejs, Expressjs, Mongodb..
-            </p>
-            <a
-              href="https://github.com/aman282571/bankingSystem"
-              target="_blank"
-              rel="noreferrer"
-            >
-              go to github
-            </a>
-            <a
-              href="https://basicbanksys.herokuapp.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Open Website
-            </a>
-          </div>
-        </div>
-        <div className="project">
-          <p className="heading">Expense Tracker</p>
-          <div className="desc">
-            A basic React application for tracking the expenses.
-            <p className="techstak">
-              <span>Tools:-</span>React
-            </p>
-            <a
-              href="https://aman282571.github.io/ExpenseTracker/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Open Website
-            </a>
-          </div>
-        </div>
-        <div className="project">
-          <p className="heading">Old Portfolio</p>
-          <div className="desc">
-            My old portfolio.
-            <p className="techstak">
-              <span>Tools:-</span>
-              HTML, CSS, JS.
-            </p>
-            <a
-              href="https://aman282571.github.io/portfolio"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Open Website
-            </a>
-          </div>
-        </div>
-
-        <div className="project">
-          <p className="heading"> Simple Landing page</p>
-          <div className="desc">
-            Responsive website design.Just homepage is created by me
-            <p className="techstak">
-              <span>Tools:-</span> HTML, CSS.
-            </p>
-            <a
-              href="https://aman282571.github.io/electura"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Open Website
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		if (document.documentElement.clientWidth > 600) {
+			$('.desc').slideDown();
+			$('.heading').toggleClass('heading1');
+		}
+		$('.heading').on('click', (e) => {
+			$(e.target).toggleClass('heading1');
+			$(e.target).siblings('.desc').slideToggle();
+		});
+	}, []);
+	return (
+		<div id='projects'>
+			<h1>Projects</h1>
+			<div className='pro_container'>
+				<div className='project'>
+					<p className='heading'>Blogs App</p>
+					<div className='desc'>
+						Blogs app where user can create,edit and delete the blogs and their
+						profile. User can also see others profiles and blogs.
+						<p className='techstak'>
+							<span>Tools:-</span> React and Express.js..
+						</p>
+						<a
+							target='_blank'
+							rel='noreferrer'
+							href='https://github.com/aman282571/blogs'
+						>
+							Go to Github
+						</a>
+					</div>
+				</div>
+				<div className='project'>
+					<p className='heading'>Fan Club portal</p>
+					<div className='desc'>
+						Portal includes authentication and Oauth, profile page(user can edit
+						info),leaderboard (to show the most active user),chat room(admin can
+						change the group fields and make others admin + real time chat +
+						highlight the unread msgs). User can make room,follow and unfollow
+						rooms.
+						<p className='techstak'>
+							<span>Tools:-</span> HTML, CSS, JS, jquery, Nodejs, Expressjs,
+							Mongodb, and socket.io..
+						</p>
+						<a
+							target='_blank'
+							rel='noreferrer'
+							href='https://github.com/aman282571/fan_club_portal'
+						>
+							go to github
+						</a>
+					</div>
+				</div>
+				<div className='project'>
+					<p className='heading'>Codeforces Problems</p>
+					<div className='desc'>
+						User can view all problems of codeforces. <br /> Features includes
+						pagination and filter by diffeent options like:-tags,
+						status(Todo,tried solved), name,increasing and decreasing order of
+						rating and solved count.
+						<p className='techstak'>
+							<span>Tools:-</span> React..
+						</p>
+						<a
+							target='_blank'
+							rel='noreferrer'
+							href='https://aman282571.github.io/cf_problems/'
+						>
+							Open website
+						</a>
+					</div>
+				</div>
+				<div className='project'>
+					<p className='heading'>Sudoku Solver</p>
+					<div className='desc'>
+						This sudoku solver takes the input from the user and validate the
+						sudoku and if any error found then it highlights those errors
+						otherwise shows all possible solution of that sudoku.
+						<p className='techstak'>
+							<span>Tools:-</span> React..
+						</p>
+						<a
+							target='_blank'
+							rel='noreferrer'
+							href='https://aman282571.github.io/sudoku'
+						>
+							Open website
+						</a>
+					</div>
+				</div>
+				<div className='project'>
+					<p className='heading'>E-Library</p>
+					<div className='desc'>
+						Library which includes registration, login page for each user. Users
+						can add, search,borrow(owner of the book will get mail),delete books
+						from this. Individual webpages to show each book.
+						<p className='techstak'>
+							<span>Tools:-</span>
+							HTML, CSS, JS, Nodejs, Expressjs, Mongodb..
+						</p>
+						<a
+							href='https://github.com/aman282571/library'
+							target='_blank'
+							rel='noreferrer'
+						>
+							go to github
+						</a>
+					</div>
+				</div>
+				<div className='project'>
+					<p className='heading'>Basic Banking System</p>
+					<div className='desc'>
+						Basic banking system which supports transaction between multiple
+						users.
+						<p className='techstak'>
+							<span>Tools:-</span>
+							HTML, CSS, jquery, Nodejs, Expressjs, Mongodb..
+						</p>
+						<a
+							href='https://github.com/aman282571/bankingSystem'
+							target='_blank'
+							rel='noreferrer'
+						>
+							go to github
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Projects;
